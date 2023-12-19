fd7804819ad2fb6326f71fe6a8feeacf28ec81f7
jaywebb67 <66364757+jaywebb67@users.noreply.github.com>
2023-12-19 12:03:04 +0000

    Completed UART Functionality
    Communication between PC and board fully functioning - also retargeted printf and scanf allowing use of both functions
    Auto Created Debug Files committed

---
+++ UART/Debug/Src/UART.d
@@ -0,0 +1,16 @@
+Src/UART.o: ../Src/UART.c ../Inc/UART.h \
+ ../Inc/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ ../Inc/CMSIS/Device/ST/STM32F4xx/Include/stm32f429xx.h \
+ ../Inc/CMSIS/Include/core_cm4.h ../Inc/CMSIS/Include/cmsis_version.h \
+ ../Inc/CMSIS/Include/cmsis_compiler.h ../Inc/CMSIS/Include/cmsis_gcc.h \
+ ../Inc/CMSIS/Include/mpu_armv7.h \
+ ../Inc/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h
+../Inc/UART.h:
+../Inc/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+../Inc/CMSIS/Device/ST/STM32F4xx/Include/stm32f429xx.h:
+../Inc/CMSIS/Include/core_cm4.h:
+../Inc/CMSIS/Include/cmsis_version.h:
+../Inc/CMSIS/Include/cmsis_compiler.h:
+../Inc/CMSIS/Include/cmsis_gcc.h:
+../Inc/CMSIS/Include/mpu_armv7.h:
+../Inc/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
---
+++ UART/Debug/Src/main.d
@@ -0,0 +1,18 @@
+Src/main.o: ../Src/main.c \
+ ../Inc/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ ../Inc/CMSIS/Device/ST/STM32F4xx/Include/stm32f429xx.h \
+ ../Inc/CMSIS/Include/core_cm4.h ../Inc/CMSIS/Include/cmsis_version.h \
+ ../Inc/CMSIS/Include/cmsis_compiler.h ../Inc/CMSIS/Include/cmsis_gcc.h \
+ ../Inc/CMSIS/Include/mpu_armv7.h \
+ ../Inc/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
+ ../Inc/UART.h ../Inc/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h
+../Inc/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+../Inc/CMSIS/Device/ST/STM32F4xx/Include/stm32f429xx.h:
+../Inc/CMSIS/Include/core_cm4.h:
+../Inc/CMSIS/Include/cmsis_version.h:
+../Inc/CMSIS/Include/cmsis_compiler.h:
+../Inc/CMSIS/Include/cmsis_gcc.h:
+../Inc/CMSIS/Include/mpu_armv7.h:
+../Inc/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
+../Inc/UART.h:
+../Inc/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
